541662945139d829c33904909a6ced8d390a5e65
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-18 21:33:23 -0500

    Log events to mysql
    Added mysql to packages
    Added start action to npm
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+const Secrets = require("./secrets.js");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+const mysql = require("mysql");
+const sql_pool = mysql.createPool({
+	connectionLimit: 5,
+	host: "localhost",
+	user: "avabur-bot",
+	password: Secrets.sql_pass,
+	database: "avabur"
+});
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
-	if (msg.content === "ping") {
-		console.log(msg.author.id);
+	if (msg.author.id !== client.user.id) console.log(msg.author.id);
+	if (msg.content === "!ping") {
 		msg.reply("pong");
 	}
+	if (msg.content === "Everyone An event is starting soon!" && msg.author.bot) {
+		console.log("Received Event starting message");
+		sql_pool.query("insert into events (time) values (current_timestamp())", function (err, _unused) {
+			if (err) throw err;
+			console.log("Logged current time in events table");
+		});
+	}
 });
-
-const Secrets = require("./secrets.js");
 
 client.login(Secrets.bot_token);
