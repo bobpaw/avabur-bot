0c705aafb64ae687971714bcb85fb71ef1eb98f7
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-22 16:57:03 -0500

    Always return hashname if not a tag
    Only execute files in test with mocha

--- test/get_version.js
+++ test/get_version.js
@@ -1,12 +1,12 @@
-const {describe, it} = require("mocha");
-const {expect} = require("chai");
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
 const valid = require("semver/functions/valid");
 const getVersion = require("../lib/get-version.js");
 
 describe("getVersion()", function () {
-	it("should be a semver, 'experimental', or commit hash", async function () {
+	it("should return a semver or a commit hash", async function () {
 		expect(await getVersion()).to.satisfy(function (str) {
-			return valid(str) !== null || "experimental" === str || /[0-9a-f]{7}/.test(str);
+			return valid(str) !== null || /[0-9a-f]{7}/.test(str);
 		});
 	});
 });
