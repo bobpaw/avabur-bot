12387167f8a9605ac3ca76abdd7644e1d586c366
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-22 11:51:15 -0500

    Use semver in test instead of long regex (which may be used underneath)
    Update it() to better describe what the test is
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- test/get_version.js
+++ test/get_version.js
@@ -1,11 +1,12 @@
 const {describe, it} = require("mocha");
 const {expect} = require("chai");
+const valid = require("semver/functions/valid");
 const getVersion = require("../lib/get-version.js");
 
 describe("getVersion()", function () {
-	it("should return a string", async function () {
+	it("should be a semver, 'experimental', or commit hash", async function () {
 		expect(await getVersion()).to.satisfy(function (str) {
-			return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/.test(str) || "experimental" === str || /[0-9a-f]{7}/.test(str);
+			return valid(str) !== null || "experimental" === str || /[0-9a-f]{7}/.test(str);
 		});
 	});
 });
