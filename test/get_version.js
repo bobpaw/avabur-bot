660f2d393e10627bf184a22eec7d31cc2fad4afa
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-22 11:44:32 -0500

    Use chai for assertions
    Use default travis script
    Also check latest node version
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- test/get_version.js
+++ test/get_version.js
@@ -1,9 +1,11 @@
 const {describe, it} = require("mocha");
-const assert = require("assert");
+const {expect} = require("chai");
 const getVersion = require("../lib/get-version.js");
 
 describe("getVersion()", function () {
 	it("should return a string", async function () {
-		assert.equal(typeof (await getVersion()), "string");
+		expect(await getVersion()).to.satisfy(function (str) {
+			return /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/.test(str) || "experimental" === str || /[0-9a-f]{7}/.test(str);
+		});
 	});
 });
