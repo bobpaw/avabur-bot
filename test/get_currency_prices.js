dbf1bc8145f2770fd8f1540b4c68d19ad96f596a
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-22 19:13:35 -0500

    Describe test better

--- test/get_currency_prices.js
+++ test/get_currency_prices.js
@@ -3,8 +3,8 @@ const { expect } = require("chai");
 const get_currency_prices = require("../lib/get-currency-prices.js");
 
 describe("get_currency_prices()", function () {
-	it("should have the key Crystal", async function () {
+	it("should return an object with the keys Crystal, Platinum, Food, Wood, Iron, Stone, Gem Fragment, and Crafting Material", async function () {
 		const currency_prices = await get_currency_prices();
-		expect(currency_prices).to.be.an("object").that.has.all.keys("Crystal", "Platinum", "Wood", "Food", "Iron", "Stone", "Gem Fragment", "Crafting Material");
+		expect(currency_prices).to.be.an("object").that.has.all.keys("Crystal", "Platinum", "Food", "Wood", "Iron", "Stone", "Gem Fragment", "Crafting Material");
 	});
 });
