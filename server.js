30618b4453e85bbe1fee44ac4bddd57ae5454992
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-29 18:06:31 -0500

    Fixed error with edits
    Before the fix it would always reply to edited messages, even if they didn't contain a command.

--- server.js
+++ server.js
@@ -27,11 +27,20 @@ client.on("message", async (msg) => {
 
 client.on("messageUpdate", async (old, new_msg) => {
 	if (new_msg.author.id === client.user.id) return;
-	if (old.id in command_messages) {
-		command_messages[new_msg.id] = await command_messages[old.id].edit(
-			`<@${command_messages[old.id].mentions.users.first().id}>, ` + await handle_message(new_msg));
-	} else {
-		command_messages[new_msg.id] = await new_msg.reply(await handle_message(new_msg));
+	let reply_text = await handle_message(new_msg);
+	try {
+		if (typeof reply_text !== "undefined" && reply_text !== "") {
+			if (old.id in command_messages) {
+				command_messages[new_msg.id] = await command_messages[old.id].edit(`<@${command_messages[old.id].mentions.users.first().id}>, ${reply_text}`);
+			} else {
+				command_messages[new_msg.id] = await new_msg.reply(reply_text);
+			}
+		} else if (old.id in command_messages) {
+			console.log("Deleting message ${command_messages[old.id].id}");
+			command_messages[old.id].delete();
+		}
+	} catch (e) {
+		console.error("Error editing/deleting/replying to message: %s", e.message);
 	}
 });
 
