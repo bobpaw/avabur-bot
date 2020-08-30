950bed5def494648aeb1ce36123c4b136f27b9be
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-29 19:41:06 -0500

    Install secrets sample if secrets.js doesn't exist
    Use npm prepare

---
+++ prepare-secrets.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+fs.access("secrets.js", function (err) {
+  if (err) {
+    fs.copyFile("secrets.sample.js", "secrets.js", fs.constants.COPYFILE_EXCL, (e) => {if (e) throw e; });
+  }
+});
