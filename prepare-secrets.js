5d2b9fad6cfa75159b5ffdadc94fff3c1cfb1f8a
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-08-31 16:31:16 -0500

    Log whether or not prepare installs a secrets file
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- prepare-secrets.js
+++ prepare-secrets.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 
 fs.access("secrets.js", function (err) {
 	if (err) {
+		console.log("Installing sample secrets.js file so that tests pass.");
 		fs.copyFile("secrets.sample.js", "secrets.js", fs.constants.COPYFILE_EXCL, (e) => { if (e) throw e; });
+	} else {
+		console.log("Not overwriting current secrets.js file.");
 	}
 });
