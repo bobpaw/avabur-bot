cd03ef0110c523f27d19099d3d462f69057af8df
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-09-02 20:45:20 -0500

    Full coverage, but may have changes before release

--- test/server.test.js
+++ test/server.test.js
@@ -6,14 +6,21 @@ const sinon = require("sinon");
 const proxyquire = require("proxyquire");
 
 const event_handlers = {};
+async function call_handlers (event, ...args) {
+	for (let handler of event_handlers[event]) {
+		await handler(...args);
+	}
+}
 let login_stub = sinon.stub().resolves("bot_token");
+let this_user = { id: 1, tag: "avabur-bot#0000", username: "avabur-bot", bot: true };
+let response_stub = sinon.stub().resolves("Response");
 
 describe("server.js", function () {
 	proxyquire("../server.js", {
 		"discord.js": {
 			Client: function () {
 				this.login = login_stub;
-				this.user = { id: 1, tag: "avabur-bot#0000", username: "avabur-bot", bot: true };
+				this.user = this_user;
 				this.on = function (event, handler) {
 					if (!(event in event_handlers)) event_handlers[event] = [];
 					event_handlers[event].push(handler);
@@ -25,7 +32,7 @@ describe("server.js", function () {
 			sql_pass: "password"
 		},
 		"./lib/commands": {
-			"handle_message": () => "Response",
+			"handle_message": response_stub,
 			"@noCallThru": true
 		}
 	});
@@ -35,6 +42,19 @@ describe("server.js", function () {
 		author: { id: 13, tag: "Wumpus#0000", username: "Wumpus", bot: false },
 		content: "",
 		reply: sinon.stub()
+	};
+	let replyMessage = {
+		id: 8,
+		author: this_user,
+		content: "",
+		edit: sinon.stub(),
+		reply: sinon.stub(),
+		delete: sinon.stub(),
+		mentions: {
+			users: {
+				first: () => fakeMessage.author
+			}
+		}
 	};
 	let log_stub, error_stub;
 	before(function () {
@@ -46,6 +66,7 @@ describe("server.js", function () {
 	});
 	afterEach(function () {
 		fakeMessage.reply.reset();
+		replyMessage.edit.reset();
 		log_stub.reset();
 		error_stub.reset();
 	});
@@ -53,14 +74,71 @@ describe("server.js", function () {
 		console.log.restore();
 		console.error.restore();
 	});
+	it("should only have one event handler per event", function () {
+		expect(event_handlers["ready"].length, "More than one ready handler").to.equal(1);
+		expect(event_handlers["message"].length, "More than one message handler").to.equal(1);
+		expect(event_handlers["messageUpdate"].length, "More than one messageUpdate handler").to.equal(1);
+	});
 	it("should error when reply throws", async function () {
 		fakeMessage.reply.rejects(new Error("Zesty testy"));
 		
-		// Call all message handlers
-		for (let handler of event_handlers["message"]) {
-			await handler(fakeMessage);
-		}
+		await call_handlers("message", fakeMessage);
 		expect(error_stub.calledOnceWithExactly("Error replying to message: %s", "Zesty testy")).to.be.true;
+	});
+	it("should return nothing and log reply id on success", async function () {
+		fakeMessage.reply.resolves(replyMessage);
+
+		// Call all message handlers
+		await call_handlers("message", fakeMessage);
+		expect(fakeMessage.reply.calledWith("Response"), "msg.reply not called with correct arguments").to.be.true;
+		expect(log_stub.calledWithExactly(`Message id of our reply: ${replyMessage.id}`), "console.log not called").to.be.true;
+	});
+	it("should not try to process my own reply", async function () {
+		await call_handlers("message", replyMessage);
+
+		expect(fakeMessage.reply.notCalled).to.be.true;
+		expect(log_stub.notCalled).to.be.true;
+		expect(error_stub.notCalled).to.be.true;
+	});
+	it("should edit its response", async function () {
+		let newMessage = {...fakeMessage};
+		response_stub.resolves("Updated response");
+		replyMessage.edit.resolves(replyMessage);
+		await call_handlers("messageUpdate", fakeMessage, newMessage);
+
+		expect(replyMessage.edit.calledWithExactly("<@13>, Updated response"), "edit not called with correct arguments").to.be.true;
+		expect(log_stub.calledWithExactly("Message id of our edit: 8"), "console.log not called").to.be.true;
+	});
+	it("should not try to process it's own update", async function () {
+		await call_handlers("messageUpdate", replyMessage, replyMessage);
+
+		expect(replyMessage.edit.notCalled).to.be.true;
+		expect(replyMessage.reply.notCalled).to.be.true;
+		expect(replyMessage.delete.notCalled).to.be.true;
+		expect(log_stub.notCalled).to.be.true;
+		expect(error_stub.notCalled).to.be.true;
+	});
+	it("should delete replies when the new response is empty", async function () {
+		response_stub.resolves();
+		await call_handlers("messageUpdate", fakeMessage, fakeMessage);
+
+		expect(replyMessage.delete.calledOnce).to.be.true;
+		expect(log_stub.calledWithExactly("Deleting message 8")).to.be.true;
+	});
+	it("should reply to messages with a new response", async function () {
+		response_stub.resolves("An interesting new response");
+		fakeMessage.reply.resolves(replyMessage);
+		await call_handlers("messageUpdate", fakeMessage, fakeMessage);
+
+		expect(fakeMessage.reply.calledWithExactly("An interesting new response")).to.be.true;
+		expect(log_stub.calledWithExactly("Message id of our reply: 8"), "Reply not called with correct argument").to.be.true;
+	});
+	it("should reply with error message on exceptions", async function () {
+		replyMessage.delete.throws(new Error("Zesty testy"));
+		response_stub.resolves();
+		await call_handlers("messageUpdate", fakeMessage, fakeMessage);
+
+		expect(error_stub.calledWithExactly("Error editing/deleting/replying to message: %s", "Zesty testy")).to.be.true;
 	});
 	it("should have only called login once", function () {
 		expect(event_handlers["ready"].length).to.equal(1);
