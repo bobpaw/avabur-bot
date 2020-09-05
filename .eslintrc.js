a073ad790365c28367c5fd9c010cea773d99d404
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-09-05 17:59:25 -0500

    Added eslint-plugin-jsdoc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,10 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 11
 	},
+	plugins: [
+		"jsdoc"
+	],
+	ignorePatterns: [".eslintrc.js"],
 	rules: {
 		"indent": [
 			"error",
@@ -66,6 +70,41 @@ module.exports = {
 				before: true,
 				after: true
 			}
-		]
+		],
+		"jsdoc/check-access": "error",
+		"jsdoc/check-alignment": "error", // Recommended
+		"jsdoc/check-examples": "warn",
+		"jsdoc/check-indentation": "error",
+		"jsdoc/check-param-names": "error", // Recommended
+		"jsdoc/check-syntax": "error",
+		"jsdoc/check-tag-names": "error", // Recommended
+		"jsdoc/check-types": "error", // Recommended
+		"jsdoc/empty-tags": "error",
+		"jsdoc/implements-on-classes": "error", // Recommended
+		"jsdoc/match-description": "off",
+		"jsdoc/newline-after-description": "error", // Recommended
+		"jsdoc/no-bad-blocks": "error",
+		"jsdoc/no-types": "off", // Only 1 if using TypeScript
+		"jsdoc/no-undefined-types": "error", // Recommended
+		"jsdoc/require-description": "error",
+		"jsdoc/require-description-complete-sentence": "warn",
+		"jsdoc/require-example": "warn",
+		"jsdoc/require-file-overview": "warn",
+		"jsdoc/require-hyphen-before-param-description": "off",
+		"jsdoc/require-jsdoc": "warn", // Recommended
+		"jsdoc/require-param": "error", // Recommended
+		"jsdoc/require-param-description": "error", // Recommended
+		"jsdoc/require-param-name": "error", // Recommended
+		"jsdoc/require-param-type": "error", // Recommended
+		"jsdoc/require-property": "error",
+		"jsdoc/require-property-description": "error",
+		"jsdoc/require-property-name": "error",
+		"jsdoc/require-property-type": "error",
+		"jsdoc/require-returns": "error", // Recommended
+		"jsdoc/require-returns-check": "error", // Recommended
+		"jsdoc/require-returns-description": "error", // Recommended
+		"jsdoc/require-returns-type": "error", // Recommended
+		"jsdoc/require-throws": "error",
+		"jsdoc/valid-types": "error" // Recommended
 	}
 };
