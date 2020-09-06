21c9d392c0c65cda50de64a7cb48e9565ae5187a
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-09-05 20:28:57 -0500

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
 	plugins: [
 		"jsdoc"
 	],
-	ignorePatterns: [".eslintrc.js"],
+	ignorePatterns: ["out/"],
 	rules: {
 		"indent": [
 			"error",
@@ -73,7 +73,7 @@ module.exports = {
 		],
 		"jsdoc/check-access": "error",
 		"jsdoc/check-alignment": "error", // Recommended
-		"jsdoc/check-examples": "warn",
+		"jsdoc/check-examples": "off", // It's sort of broken right now
 		"jsdoc/check-indentation": "error",
 		"jsdoc/check-param-names": "error", // Recommended
 		"jsdoc/check-syntax": "error",
@@ -106,5 +106,25 @@ module.exports = {
 		"jsdoc/require-returns-type": "error", // Recommended
 		"jsdoc/require-throws": "error",
 		"jsdoc/valid-types": "error" // Recommended
-	}
+	},
+	overrides: [
+		{
+			files: ["*.test.js", ".eslintrc.js"],
+			rules: {
+				"jsdoc/check-access": 0, "jsdoc/check-alignment": 0, "jsdoc/check-examples": 0,
+				"jsdoc/check-indentation": 0, "jsdoc/check-param-names": 0, "jsdoc/check-syntax": 0,
+				"jsdoc/check-tag-names": 0, "jsdoc/check-types": 0, "jsdoc/empty-tags": 0,
+				"jsdoc/implements-on-classes": 0, "jsdoc/match-description": 0, "jsdoc/newline-after-description": 0,
+				"jsdoc/no-bad-blocks": 0, "jsdoc/no-types": 0, "jsdoc/no-undefined-types": 0,
+				"jsdoc/require-description": 0, "jsdoc/require-description-complete-sentence": 0,
+				"jsdoc/require-example": 0, "jsdoc/require-file-overview": 0, "jsdoc/require-hyphen-before-param-description": 0,
+				"jsdoc/require-jsdoc": 0, "jsdoc/require-param": 0, "jsdoc/require-param-description": 0,
+				"jsdoc/require-param-name": 0, "jsdoc/require-param-type": 0, "jsdoc/require-property": 0,
+				"jsdoc/require-property-description": 0, "jsdoc/require-property-name": 0,
+				"jsdoc/require-property-type": 0, "jsdoc/require-returns": 0, "jsdoc/require-returns-check": 0,
+				"jsdoc/require-returns-description": 0, "jsdoc/require-returns-type": 0,
+				"jsdoc/require-throws": 0, "jsdoc/valid-types": 0
+			}
+		}
+	]
 };
